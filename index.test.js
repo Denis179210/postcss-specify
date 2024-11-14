63a29cab32c7c70bcbf2c8c1cece29a66e42937c
Denis Bochar <denis.bochar1992@gmail.com>
2024-11-14 17:00:06 -0600

    Fixed test case for pseudo elemnts

--- index.test.js
+++ index.test.js
@@ -10,37 +10,35 @@ async function run(input, output, opts = {}) {
   equal(result.warnings().length, 0)
 }
 
-
 test('Specifies all rules', async () => {
-  const input1 = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
-  const expect1 = `#app {${input1} }`;
-  await run(input1, expect1, { 'selector': '#app' })
+  const input = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+  const expect = `#app {${input} }`;
+  await run(input, expect, { 'selector': '#app' })
 });
 
 
 test('Specifies all rules except ignore list', async () => {
-  const input2 = 'html { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
-  const expect2 = `html { font-size: 16px } #app { .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } } }`;
-  await run(input2, expect2, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
+  const input = 'html { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+  const expect = `html { font-size: 16px } #app { .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } } }`;
+  await run(input, expect, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
 });
 
 
-const input3 = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
-const expect3 = `${input3}`;
-
 test('Does nothing because of selector argument absence', async () => {
-  await run(input3, expect3);
+  const input = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+  const expect = `${input}`;
+  await run(input, expect);
 });
 
 test('Specifies all rules except ignore list and extracts ignorable selector names from multi-selectors', async () => {
-  const input4 = 'html,body,div { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
-  const expect4 = `html { font-size: 16px }body { font-size: 16px }#app {div { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } } }`;
-  await run(input4, expect4, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
+  const input = 'html,body,div { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+  const expect = `html { font-size: 16px }body { font-size: 16px }#app {div { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } } }`;
+  await run(input, expect, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
 });
 
 
 test('Keeps original indents and spaces', async () => {
-  const input5 = `
+  const input = `
     .hello,.hello-again {
       color: red;
       .inside {
@@ -48,7 +46,7 @@ test('Keeps original indents and spaces', async () => {
       }
     }
   `;
-  const expect5 = `#app {
+  const expect = `#app {
     .hello,.hello-again {
       color: red;
       .inside {
@@ -57,7 +55,13 @@ test('Keeps original indents and spaces', async () => {
     }
 }
   `;
-  await run(input5, expect5, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
+  await run(input, expect, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
+});
+
+test('Ignores pseudo elements and pseudo classes', async () => {
+  const input = `::after {content: '*'; display: block} div {color: red}`;
+  const expect = `::after {content: '*'; display: block} #app { div {color: red}}`;
+  await run(input, expect, { 'selector': '#app', 'ignore': ['html', 'body', ':host', '::before', '::after'] })
 });
 
 

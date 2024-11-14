415ddbee614c423af35aae48d04db81c24fa79cf
Denis Bochar <denis.bochar1992@gmail.com>
2024-11-14 12:26:44 -0600

    Added @ignore parameter to give an abilitiy to skip specifying some rules (e.x html, body)

--- index.js
+++ index.js
@@ -5,18 +5,46 @@ const {Rule} = require("postcss");
 module.exports = (opts = {}) => {
   // Work with options here
 
-  const specify = opts.selector;
+  const selector = opts.selector;
+  const ignoreTransformationFor = opts.ignore;
 
   return {
     postcssPlugin: 'postcss-specify',
     Once (root) {
       // console.log(`postcssPlugin "postcss-specify" started modifying node on ${root.nodes} at ${new Date().toISOString()}`);
-      const cloneRootNodes = root.nodes.map(node => node.clone());
+
+      if (!selector) {
+        console.warn('No specific selector passed! Nothing done!');
+        return;
+      }
+
+      const ignoreNodes = [];
+      const acceptNodes = [];
+
+      root.nodes.forEach((node) => {
+        const clone = node.clone();
+        if (
+            Array.isArray(ignoreTransformationFor) &&
+            !!ignoreTransformationFor.length &&
+            node instanceof Rule &&
+            ignoreTransformationFor.includes(clone.selector)
+        ) {
+          ignoreNodes.push(clone);
+        }
+        else {
+          acceptNodes.push(clone);
+        }
+
+      });
+
       root.removeAll();
+
       const specificityNode = new Rule({
-        selector: `${specify}`,
-        nodes: cloneRootNodes
+        selector: `${selector}`,
+        nodes: acceptNodes
       })
+
+      root.append(...ignoreNodes);
       root.append(specificityNode);
       // console.log(`postcssPlugin "postcss-specify": Success!`);
     }
--- index.test.js
+++ index.test.js
@@ -10,10 +10,28 @@ async function run(input, output, opts = {}) {
   equal(result.warnings().length, 0)
 }
 
-const input = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
-const expect = `#app {${input} }`;
-// const wrapped = `${}`
-test('does something', async () => {
-  await run(input, expect, { 'selector': '#app' })
-})
+const input1 = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+const expect1 = `#app {${input1} }`;
+
+test('Specifies all rules', async () => {
+  await run(input1, expect1, { 'selector': '#app' })
+});
+
+
+const input2 = 'html { font-size: 16px } .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+const expect2 = `html { font-size: 16px } #app { .hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } } }`;
+
+test('Specifies all rules except ignore list', async () => {
+  await run(input2, expect2, { 'selector': '#app', 'ignore': ['html', 'body', ':before'] })
+});
+
+
+const input3 = '.hello,.hello-again { color: red; .inside { height: 100% } } .world { color: gray } @media screen { .yes { color: blue } }';
+const expect3 = `${input3}`;
+
+test('Does nothing because of selector argument absence', async () => {
+  await run(input3, expect3);
+});
+
+
 
